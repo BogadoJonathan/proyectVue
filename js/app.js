d656195dd32cbcdbf25f4190ee86a78f55621bec
jonathan bogado <jb@bb.vision>
2022-07-11 10:46:44 -0300

    Agregando componente

--- js/app.js
+++ js/app.js
@@ -1,21 +1,50 @@
+Vue.component('mi-table',{
+    props:['alumnos'],
+    template:`
+    <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>edad</th>
+                <th>Nacionalidad</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr  v-for="(item,i) in alumnos" :key="i">
+                <td >{{item.nombre}}</td>
+                <td>{{item.edad}}</td>
+                <td>{{item.nacionalidad}}</td>
+                {{clase}}
+            </tr>
+        </tbody>
+    </table>   
+    `
+})
+
 var app = new Vue({
     el: '#app',
-    // data contiene variables
     data: {
-        totalContador: 0,
-
-    },
-    // contiene metodos/funciones
-    methods: {
-
-        restar(){
-            if (this.totalContador>0){
-                this.totalContador--
-            }
-        },
-        sumar(){
-            this.totalContador++
-        }
-    },
-
+        alumnos:[
+            {
+                'nombre'        :   'Jonathan',
+                'edad'          :   26,
+                'nacionalidad'  :   'Argentina'
+            },
+            {
+                'nombre'        :   'Andrea',
+                'edad'          :   43,
+                'nacionalidad'  :   'Argentina'
+            },
+            {
+                'nombre'        :   'Leonardo',
+                'edad'          :   32,
+                'nacionalidad'  :   'Venezuela'
+            },
+            {
+                'nombre'        :   'Gisele',
+                'edad'          :   26,
+                'nacionalidad'  :   'España'
+            },
+        ]
+    }
 })
