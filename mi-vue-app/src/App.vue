c724945360e0d183da14bc795ce5abd03f0094ca
jonathan bogado <jb@bb.vision>
2022-07-18 21:31:39 -0300

    proyecto Vue CLI

---
+++ mi-vue-app/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <MyTable 
+      :alumnos="mis_datos"
+      :new_style="estilo_verde_azul"/>
+  </div>
+</template>
+
+<script>
+import MyTable from './components/MyTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyTable
+  },
+  data(){
+    return{
+      estilo_verde_azul:'background: linear-gradient(56deg, #c4ff02 20%,  #00eaff 100%);',
+      estilo_naranja_lima:'background: linear-gradient(56deg, #e86c07 20%,  #bbff00 100%);',
+      mis_datos:[
+            {
+                'nombre'        :   'Jonathan',
+                'edad'          :   26,
+                'nacionalidad'  :   'Argentina'
+            },
+            {
+                'nombre'        :   'Andrea',
+                'edad'          :   43,
+                'nacionalidad'  :   'Argentina'
+            },
+            {
+                'nombre'        :   'Leonardo',
+                'edad'          :   32,
+                'nacionalidad'  :   'Venezuela'
+            },
+            {
+                'nombre'        :   'Gisele',
+                'edad'          :   26,
+                'nacionalidad'  :   'España'
+            },
+        ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ mi-vue-app/src/components/MyTable.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>MI TABLE</h1>
+      <table class="myTableStyle">
+            <thead :style="new_style">
+                <tr>
+                    <th>Nombre</th>
+                    <th>edad</th>
+                    <th>Nacionalidad</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr  v-for="(item,i) in alumnos" :key="i">
+                    <td >{{item.nombre}}</td>
+                    <td>{{item.edad}}</td>
+                    <td>{{item.nacionalidad}}</td>
+                    {{clase}}
+                </tr>
+            </tbody>
+        </table>   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyTable',
+  props:['alumnos','new_style']
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .myTableStyle{
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+  }
+  .myTableStyle thead{
+  text-align: center;
+  }
+
+  .myTableStyle td {
+  text-align: center ;
+  }
+
+  
+</style>
